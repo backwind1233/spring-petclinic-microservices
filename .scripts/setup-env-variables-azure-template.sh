d8db5fac2a75e65b773dcc4a9e33759e08844603
zhihaoguo <zhihaoguo@microsoft.com>
2021-09-02 21:11:25 +0800

    - update readme and shell script

--- .scripts/setup-env-variables-azure-template.sh
+++ .scripts/setup-env-variables-azure-template.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 # ==== Resource Group ====
-export SUBSCRIPTION=subscription-id # customize this
-export RESOURCE_GROUP=resource-group-name # customize this
+export SUBSCRIPTION=799c12ba-353c-44a1-883d-84808ebb2216 # customize this
+export RESOURCE_GROUP=gzh-pc-local-04-rg # customize this
 export LOCATION=SouthCentralUS  #customize this
-export COSMOSDB_NAME=mycosmosdbaccname  # customize this
-export REDIS_NAME=myredisname #customize this
-export KEYVAULT_NAME=myend2endkv #customize this
+export COSMOSDB_NAME=gzh-pc-local-04-cosmos  # customize this
+export REDIS_NAME=gzh-pc-local-04-redis #customize this
+export KEYVAULT_NAME=gzh-pc-local-04-keyvault #customize this
+export APP_NAME_FOR_KEYVAULT=local-04-appforkeyvault #customize this
+
+# ==== Create ResouceGroup ====
+az group create --name ${RESOURCE_GROUP} \
+        --location ${LOCATION}
 
 # ==== Create CosmosDB Account ====
+# note： it may take a few minutes
 az cosmosdb create --name $COSMOSDB_NAME --resource-group $RESOURCE_GROUP
 COSMOS_KEYS=$(az cosmosdb keys list --name $COSMOSDB_NAME --resource-group $RESOURCE_GROUP --type keys)
 COSMOS_PRIMARY_KEY=$(echo $COSMOS_KEYS | jq -r .primaryMasterKey)
@@ -23,21 +29,29 @@ REDIS_PASSWORD=$(az redis list-keys --name $REDIS_NAME --resource-group $RESOURC
 # ==== Create a KeyVault Account ====
 az keyvault create --location $LOCATION --name $KEYVAULT_NAME --resource-group $RESOURCE_GROUP
 
-SERVICE_PRINCIPAL=$(az ad sp create-for-rbac -n "endtoendsp")
-az keyvault set-policy -n MyVault --key-permissions get list --spn $AZURE_KEYVAULT_CLIENTID
+SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --name ${APP_NAME_FOR_KEYVAULT} --role Contributor)
 
-AZURE_KEYVAULT_URI=$(az keyvault show --name $KEYVAULT_NAME | jq -r .properties | jq -r .vaultUri)
+AZURE_KEYVAULT_URI=$(az keyvault show --name $KEYVAULT_NAME --resource-group $RESOURCE_GROUP | jq -r .properties | jq -r .vaultUri)
 AZURE_KEYVAULT_CLIENTID=$(echo $SERVICE_PRINCIPAL | jq -r .appId)
 AZURE_KEYVAULT_TENANTID=$(echo $SERVICE_PRINCIPAL | jq -r .tenant)
-AZURE_KEYVAULT_CLIENTKEY=$(echo $SERVICE_PRINCIPAL | jq -r .password)	
+AZURE_KEYVAULT_CLIENTKEY=$(echo $SERVICE_PRINCIPAL | jq -r .password)
+
+#
+az keyvault set-policy -n $KEYVAULT_NAME --key-permissions get list \
+ --certificate-permissions get list \
+ --secret-permissions get list \
+ --resource-group $RESOURCE_GROUP \
+ --spn $AZURE_KEYVAULT_CLIENTID
 
 
 # ==== add keys to keyvault ====
-az keyvault secret set --name  cosmosdburi --value  $COSMOSDB_URI
-az keyvault secret set --name  cosmosdbkey --value  $COSMOS_PRIMARY_KEY
-az keyvault secret set --name  cosmosdbsecondarykey --value  $COSMOS_SECONDARY_KEY
-az keyvault secret set --name  redisuri--value  $REDIS_HOSTNAME
-az keyvault secret set --name  redispassword --value  $REDIS_PASSWORD
+
+az keyvault secret set --vault-name $KEYVAULT_NAME --name cosmosdburi --value $COSMOSDB_URI
+az keyvault secret set --vault-name $KEYVAULT_NAME --name cosmosdbkey --value $COSMOS_PRIMARY_KEY
+az keyvault secret set --vault-name $KEYVAULT_NAME --name cosmosdbsecondarykey --value $COSMOS_SECONDARY_KEY
+az keyvault secret set --vault-name $KEYVAULT_NAME --name redisuri --value $REDIS_HOSTNAME
+az keyvault secret set --vault-name $KEYVAULT_NAME --name redispassword --value $REDIS_PASSWORD
+
 
 # ==== Create Kevvault environment file for Docker containers ====
 cat > keyvault.env << EOF
@@ -46,8 +60,3 @@ AZURE_KEYVAULT_CLIENTID=$AZURE_KEYVAULT_CLIENTID
 AZURE_KEYVAULT_TENANTID=$AZURE_KEYVAULT_TENANTID
 AZURE_KEYVAULT_CLIENTKEY=$AZURE_KEYVAULT_CLIENTKEY
 EOF
-
-
-
-
-
